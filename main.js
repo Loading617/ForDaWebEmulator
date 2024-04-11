a8c7102259345533f8c8adf9e29af9296016dc14
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-04-11 13:51:08 -0400

    Add JavaScript

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 importPackage(java.io);
 importPackage(java.lang);
+importPackage(java.util);
+importPackage(java.sql);
+importPackage(javax.swing);
+importPackage(javax.swing.border);
+importPackage(javax.swing.event);
+importPackage(javax.swing.table);
+importPackage(javax.swing.text);
+importPackage(javax.swing.tree);
+importPackage(javax.swing.filechooser);
+importPackage(javax.swing.text.html);
+importPackage(javax.swing.text.html.parser);
+importPackage(javax.swing.text.rtf);
+
+
 
 // your code goes here
