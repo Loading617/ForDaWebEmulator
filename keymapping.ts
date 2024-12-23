e623b141fcc0cdbc28e2f44ab7c62d88415730ba
Jahim Sankoh <jahimsankoh319@gmail.com>
2024-12-23 12:47:07 -0500

    Add files via upload

---
+++ keymapping.ts
@@ -0,0 +1,14 @@
+// Default key mappings
+const defaultControls = {
+    UP: "ArrowUp",
+    DOWN: "ArrowDown",
+    LEFT: "ArrowLeft",
+    RIGHT: "ArrowRight",
+    BUTTON_A: "KeyZ",
+    BUTTON_B: "KeyX",
+    START: "Enter",
+    SELECT: "Shift",
+ };
+ 
+ type ControlMapping = typeof defaultControls;
+ 
