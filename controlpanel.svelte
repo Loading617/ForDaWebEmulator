548337c8b935f7391b65d31b1cd77dccb35f97bb
Jahim Sankoh <jahimsankoh319@gmail.com>
2024-12-23 12:35:14 -0500

    Update controlpanel.svelte

--- controlpanel.svelte
+++ controlpanel.svelte
@@ -2,11 +2,15 @@
     export let onStart: () => void;
     export let onPause: () => void;
     export let onReset: () => void;
+    export let onSaveState: () => void;
+    export let onLoadState: () => void;
  </script>
  
  <div>
     <button on:click={onStart}>Start</button>
     <button on:click={onPause}>Pause</button>
     <button on:click={onReset}>Reset</button>
+    <button on:click={onSaveState}>Save State</button>
+    <button on:click={onLoadState}>Load State</button>
  </div>
  
