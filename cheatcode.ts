6d924b5abf665ba4d6fe071ff96a620c14322497
Jahim Sankoh <jahimsankoh319@gmail.com>
2024-12-23 13:15:21 -0500

    Add files via upload

---
+++ cheatcode.ts
@@ -0,0 +1,9 @@
+type CheatCode = {
+    id: string; // Unique identifier
+    description: string; // Short description of the cheat
+    code: string; // The actual cheat code
+    enabled: boolean; // Whether the cheat is active
+ };
+ 
+ let cheats: CheatCode[] = [];
+ 
