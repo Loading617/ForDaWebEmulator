2b19269586cd04aef665eaf68d51b9f700540290
Jahim Sankoh <jahimsankoh319@gmail.com>
2024-12-23 14:54:18 -0500

    Update app.svelte

--- app.svelte
+++ app.svelte
@@ -4,10 +4,12 @@
     import FileUploader from "./FileUploader.svelte";
     import ControlPanel from "./ControlPanel.svelte";
     import ControlSettings from "./ControlSettings.svelte";
+    import Cheats Managerimport Cheatmanager from "./cheatmanager.svelte";
  
     let emulator: any;
     let saveStateData: Uint8Array | null = null;
     let controls = { ...defaultControls };
+    let cheats: CheatCode[] = [];
 
     const initializeEmulator = (romData: ArrayBuffer) => {
        emulator.loadROM(romData);
@@ -65,6 +67,31 @@
     };
  
     window.addEventListener("keydown", handleKeyPress);
+
+    const addCheat = (cheat: CheatCode) => {
+      cheats = [...cheats, cheat];
+      if (emulator) {
+         emulator.addCheat(cheat.code, cheat.enabled); // Emulator integration
+      }
+   };
+
+   const toggleCheat = (id: string, enabled: boolean) => {
+      cheats = cheats.map((cheat) =>
+         cheat.id === id ? { ...cheat, enabled } : cheat
+      );
+      const cheat = cheats.find((cheat) => cheat.id === id);
+      if (cheat && emulator) {
+         emulator.toggleCheat(cheat.code, enabled); // Emulator integration
+      }
+   };
+
+   const removeCheat = (id: string) => {
+      const cheat = cheats.find((cheat) => cheat.id === id);
+      if (cheat && emulator) {
+         emulator.removeCheat(cheat.code); // Emulator integration
+      }
+      cheats = cheats.filter((cheat) => cheat.id !== id);
+   };
  </script>
  
  <main>
@@ -79,5 +106,10 @@
     onLoadState={loadState}/>
     <h2>Control Settings</h2>
     <ControlSettings {controls} onChange={updateControls} />
+    <CheatManager
+    {cheats}
+      onAddCheat={addCheat}
+      onToggleCheat={toggleCheat}
+      onRemoveCheat={removeCheat}/>
  </main>
  
