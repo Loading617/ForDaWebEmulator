0153471752534eab7bc6f8ffe6225ce0644751be
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-04-10 13:59:32 -0400

    Update addcheat.ts

--- addcheat.ts
+++ addcheat.ts
@@ -1,12 +1,9 @@
 emulator.addCheat = (code: string, enabled: boolean) => {
-    // Add cheat logic (e.g., parsing and applying cheat code)
  };
  
  emulator.toggleCheat = (code: string, enabled: boolean) => {
-    // Enable or disable cheat code
  };
  
  emulator.removeCheat = (code: string) => {
-    // Remove cheat logic
  };
  
