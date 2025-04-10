b76a533fa74c6a12788238713c90a8daeb76dcdc
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-04-10 14:09:05 -0400

    Update wasmmodule.ts

--- wasmmodule.ts
+++ wasmmodule.ts
@@ -1,6 +1,5 @@
 let saveState: () => Uint8Array;
 let loadState: (state: Uint8Array) => void;
 
-// Assign them after initializing the WASM module:
 saveState = emulatorModule.save_state;
 loadState = emulatorModule.load_state;
